8e2c1e0c9c8ee7064fbf52aa18662324e5fdeb4d
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-11-13 14:14:16 +0100

    Add .NET Core example

---
+++ ConsoleAppCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleAppCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 9;
+            int t = y / 0;
+
+            X(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+            // int y  = 99;
+        }
+
+        static void X(int x, int x1, int x2, int x11, int x111, int x1111, int x0, int x00, int x0d0, int x800, int x000, int xa, int xai, int xab, int xaa, int xam, int xas, int xdas)
+        {
+
+        }
+    }
+}
